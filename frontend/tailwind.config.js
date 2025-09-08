9667bb0bfcc06ff4887ecbef196e9feed93830e6
AXsavag <muktaribro13@gmail.com>
2025-09-08 17:24:41 +0100

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#4f46e5", // Indigo
+        secondary: "#06b6d4", // Cyan
+        accent: "#f59e0b" // Amber
+      },
+    },
   },
-  plugins: [],
-};
+  plugins: [require("@tailwindcss/typography")],
+}
